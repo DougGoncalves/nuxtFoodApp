e5969455dfd6b539c684045a51006f2578f4c2a4
DougGoncalves <doug.goncalvesf@gmail.com>
2021-03-24 15:51:03 -0300

    First commit

---
+++ layouts/default.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family:
+    'Mulish',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+    font-weight: 300;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+h1, h2, h3, h4 {
+  font-family:
+    'Poppins',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+    font-weight: 600;
+
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="container">
+    <h1>Hi from home page</h1>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
